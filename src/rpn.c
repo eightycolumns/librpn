c076f538adc2917517713f1880a374db87ea8f1a
Drew Keller <drew@eightycolumns.com>
2016-10-05 06:44:07 -0400

    Check that infix src isn't NULL

--- src/rpn.c
+++ src/rpn.c
@@ -3,9 +3,7 @@
 #include <stddef.h>
 
 int infix_to_postfix(char *postfix, const char *infix) {
-  (void)infix; // Suppress "unused parameter" warning
-
-  if (postfix == NULL) {
+  if (postfix == NULL || infix == NULL) {
     return RPN_NULL_POINTER_ERROR;
   }
 
--- test/null-pointer-error-tcase.c
+++ test/null-pointer-error-tcase.c
@@ -10,10 +10,17 @@ START_TEST(postfix_dest_cannot_be_NULL) {
   ck_assert_int_eq(expected, actual);
 } END_TEST
 
+START_TEST(infix_src_cannot_be_NULL) {
+  int expected = RPN_NULL_POINTER_ERROR;
+  int actual = infix_to_postfix("", NULL);
+  ck_assert_int_eq(expected, actual);
+} END_TEST
+
 TCase *null_pointer_error_tcase(void) {
   TCase *null_pointer_error_tcase = tcase_create("Null Pointer Error");
 
   tcase_add_test(null_pointer_error_tcase, postfix_dest_cannot_be_NULL);
+  tcase_add_test(null_pointer_error_tcase, infix_src_cannot_be_NULL);
 
   return null_pointer_error_tcase;
 }
