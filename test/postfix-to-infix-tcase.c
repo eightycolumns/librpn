28f677d4b9e1ffb87e311cdc0bd44a95229e5898
Drew Keller <drew@eightycolumns.com>
2016-10-07 19:06:49 -0400

    Convert ab-c+ to a-b+c

--- test/postfix-to-infix-tcase.c
+++ test/postfix-to-infix-tcase.c
@@ -20,10 +20,25 @@ START_TEST(test_01) {
   );
 } END_TEST
 
+START_TEST(test_02) {
+  char input[] = "ab-c+";
+  char expected_output[] = "a-b+c";
+  char actual_output[strlen(expected_output) + 1];
+
+  postfix_to_infix(actual_output, input);
+
+  ck_assert_msg(
+    strcmp(expected_output, actual_output) == 0,
+    "Input \"%s\". Expected output \"%s\", got \"%s\".",
+    input, expected_output, actual_output
+  );
+} END_TEST
+
 TCase *postfix_to_infix_tcase(void) {
   TCase *postfix_to_infix_tcase = tcase_create("Postfix to Infix");
 
   tcase_add_test(postfix_to_infix_tcase, test_01);
+  tcase_add_test(postfix_to_infix_tcase, test_02);
 
   return postfix_to_infix_tcase;
 }
