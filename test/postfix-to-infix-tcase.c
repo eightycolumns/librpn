feeb0af49af1adc402061560603fe76f94bebb2f
Drew Keller <drew@eightycolumns.com>
2016-10-07 19:10:00 -0400

    Extract test_postfix_to_infix()

--- test/postfix-to-infix-tcase.c
+++ test/postfix-to-infix-tcase.c
@@ -6,9 +6,10 @@
 
 #include "src/rpn.h"
 
-START_TEST(test_01) {
-  char input[] = "ab+c-";
-  char expected_output[] = "a+b-c";
+static void test_postfix_to_infix(
+  const char *expected_output,
+  const char *input
+) {
   char actual_output[strlen(expected_output) + 1];
 
   postfix_to_infix(actual_output, input);
@@ -18,20 +19,14 @@ START_TEST(test_01) {
     "Input \"%s\". Expected output \"%s\", got \"%s\".",
     input, expected_output, actual_output
   );
+}
+
+START_TEST(test_01) {
+  test_postfix_to_infix("a+b-c", "ab+c-");
 } END_TEST
 
 START_TEST(test_02) {
-  char input[] = "ab-c+";
-  char expected_output[] = "a-b+c";
-  char actual_output[strlen(expected_output) + 1];
-
-  postfix_to_infix(actual_output, input);
-
-  ck_assert_msg(
-    strcmp(expected_output, actual_output) == 0,
-    "Input \"%s\". Expected output \"%s\", got \"%s\".",
-    input, expected_output, actual_output
-  );
+  test_postfix_to_infix("a-b+c", "ab-c+");
 } END_TEST
 
 TCase *postfix_to_infix_tcase(void) {
