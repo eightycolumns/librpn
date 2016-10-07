9c0a878c9ed9c1a0ecba07e25fa28dca1bf86cb5
Drew Keller <dkeller@pillartechnology.com>
2016-10-07 16:12:06 -0400

    Convert a^b*c/d+e-f to ab^c*d/e+f-

--- test/infix-to-postfix-tcase.c
+++ test/infix-to-postfix-tcase.c
@@ -49,6 +49,10 @@ START_TEST(test_07) {
   test_infix_to_postfix("ab+cd*ef^/-", "a+b-c*d/e^f");
 } END_TEST
 
+START_TEST(test_08) {
+  test_infix_to_postfix("ab^c*d/e+f-", "a^b*c/d+e-f");
+} END_TEST
+
 TCase *infix_to_postfix_tcase(void) {
   TCase *infix_to_postfix_tcase = tcase_create("Infix to Postfix");
 
@@ -59,6 +63,7 @@ TCase *infix_to_postfix_tcase(void) {
   tcase_add_test(infix_to_postfix_tcase, test_05);
   tcase_add_test(infix_to_postfix_tcase, test_06);
   tcase_add_test(infix_to_postfix_tcase, test_07);
+  tcase_add_test(infix_to_postfix_tcase, test_08);
 
   return infix_to_postfix_tcase;
 }
