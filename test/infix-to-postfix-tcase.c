7ec853221de24b7f22de5335913ae56c1f362cc7
Drew Keller <dkeller@pillartechnology.com>
2016-10-07 15:11:21 -0400

    Convert a/b*c to ab/c*

--- test/infix-to-postfix-tcase.c
+++ test/infix-to-postfix-tcase.c
@@ -33,12 +33,17 @@ START_TEST(test_03) {
   test_infix_to_postfix("ab*c/", "a*b/c");
 } END_TEST
 
+START_TEST(test_04) {
+  test_infix_to_postfix("ab/c*", "a/b*c");
+} END_TEST
+
 TCase *infix_to_postfix_tcase(void) {
   TCase *infix_to_postfix_tcase = tcase_create("Infix to Postfix");
 
   tcase_add_test(infix_to_postfix_tcase, test_01);
   tcase_add_test(infix_to_postfix_tcase, test_02);
   tcase_add_test(infix_to_postfix_tcase, test_03);
+  tcase_add_test(infix_to_postfix_tcase, test_04);
 
   return infix_to_postfix_tcase;
 }
