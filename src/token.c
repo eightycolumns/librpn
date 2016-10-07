fcb2747068ca73c94520c3092b1f7fc9716927f3
Drew Keller <dkeller@pillartechnology.com>
2016-10-07 16:36:06 -0400

    Simplify is_operand()

--- src/token.c
+++ src/token.c
@@ -1,20 +1,13 @@
 #include "src/token.h"
 
 #include <assert.h>
+#include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
 
 bool is_operand(const char *token) {
   assert(token != NULL);
-
-  return (
-    strcmp("a", token) == 0 ||
-    strcmp("b", token) == 0 ||
-    strcmp("c", token) == 0 ||
-    strcmp("d", token) == 0 ||
-    strcmp("e", token) == 0 ||
-    strcmp("f", token) == 0
-  );
+  return strlen(token) == 1 && islower(token[0]);
 }
 
 bool is_operator(const char *token) {
