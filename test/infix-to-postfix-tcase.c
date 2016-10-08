5bee46e5ee43cab98f17e98b655d2170075446bd
Drew Keller <drew@eightycolumns.com>
2016-10-08 10:09:45 -0400

    Convert a*(b^c+d) to abc^d+*

--- test/infix-to-postfix-tcase.c
+++ test/infix-to-postfix-tcase.c
@@ -77,6 +77,10 @@ START_TEST(test_14) {
   test_infix_to_postfix("abc^+d*", "(a+b^c)*d");
 } END_TEST
 
+START_TEST(test_15) {
+  test_infix_to_postfix("abc^d+*", "a*(b^c+d)");
+} END_TEST
+
 TCase *infix_to_postfix_tcase(void) {
   TCase *infix_to_postfix_tcase = tcase_create("Infix to Postfix");
 
@@ -94,6 +98,7 @@ TCase *infix_to_postfix_tcase(void) {
   tcase_add_test(infix_to_postfix_tcase, test_12);
   tcase_add_test(infix_to_postfix_tcase, test_13);
   tcase_add_test(infix_to_postfix_tcase, test_14);
+  tcase_add_test(infix_to_postfix_tcase, test_15);
 
   return infix_to_postfix_tcase;
 }
