3fdabbae12152d7edb8ebc3db3f8009d46b025ee
Drew Keller <drew@eightycolumns.com>
2016-10-08 21:59:18 -0400

    Reject a*b+c) as an infix expression

--- test/malformed-infix-expression-tcase.c
+++ test/malformed-infix-expression-tcase.c
@@ -45,6 +45,10 @@ START_TEST(test_07) {
   test_for_malformed_expression_error("a*(b+c");
 } END_TEST
 
+START_TEST(test_08) {
+  test_for_malformed_expression_error("a*b+c)");
+} END_TEST
+
 TCase *malformed_infix_expression_tcase(void) {
   TCase *malformed_infix_expression_tcase = tcase_create(
     "Malformed Infix Expression"
@@ -57,6 +61,7 @@ TCase *malformed_infix_expression_tcase(void) {
   tcase_add_test(malformed_infix_expression_tcase, test_05);
   tcase_add_test(malformed_infix_expression_tcase, test_06);
   tcase_add_test(malformed_infix_expression_tcase, test_07);
+  tcase_add_test(malformed_infix_expression_tcase, test_08);
 
   return malformed_infix_expression_tcase;
 }
