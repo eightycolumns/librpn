6d0d7dabaa80a1a90b1d98870ca4ca6afb9030e1
Drew Keller <drew@eightycolumns.com>
2016-10-09 14:00:59 -0400

    Split rpn.c into {infix,postfix}-conversion.c

---
+++ src/infix-conversion.c
@@ -0,0 +1,86 @@
+#include "src/rpn.h"
+
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "src/associativity.h"
+#include "src/infix-validation.h"
+#include "src/precedence.h"
+#include "src/stack.h"
+#include "src/token.h"
+#include "src/util.h"
+
+static bool stack_takes_precedence(const Stack *stack, const char *operator);
+
+int infix_to_postfix(char *postfix, const char *infix) {
+  if (postfix == NULL || infix == NULL) {
+    return RPN_NULL_POINTER_ERROR;
+  }
+
+  if (!is_valid_infix_expression(infix)) {
+    return RPN_MALFORMED_EXPRESSION_ERROR;
+  }
+
+  strcpy(postfix, "");
+
+  Stack *stack = NULL;
+
+  size_t infix_length = strlen(infix);
+
+  for (size_t i = 0; i < infix_length; i += 1) {
+    char token[2];
+    copy_substring(token, infix + i, 1);
+
+    if (is_operand(token)) {
+      strcat(postfix, token);
+    } else if (is_operator(token)) {
+      while (stack_takes_precedence(stack, token)) {
+        char operator[2];
+        pop(operator, &stack);
+        strcat(postfix, operator);
+      }
+
+      if (push(&stack, token) == NULL) {
+        clear(&stack);
+        return RPN_MEMORY_ALLOCATION_ERROR;
+      }
+    } else if (is_opening_paren(token)) {
+      if (push(&stack, token) == NULL) {
+        clear(&stack);
+        return RPN_MEMORY_ALLOCATION_ERROR;
+      }
+    } else if (is_closing_paren(token)) {
+      while (!is_opening_paren(peek(stack))) {
+        char operator[2];
+        pop(operator, &stack);
+        strcat(postfix, operator);
+      }
+
+      char opening_paren[2];
+      pop(opening_paren, &stack);
+    }
+  }
+
+  while (!is_empty(stack)) {
+    char operator[2];
+    pop(operator, &stack);
+    strcat(postfix, operator);
+  }
+
+  return RPN_SUCCESS;
+}
+
+static bool stack_takes_precedence(const Stack *stack, const char *operator) {
+  assert(operator != NULL);
+
+  if (!is_empty(stack) && is_left_associative(operator)) {
+    return precedence_of(peek(stack)) >= precedence_of(operator);
+  }
+
+  if (!is_empty(stack) && is_right_associative(operator)) {
+    return precedence_of(peek(stack)) > precedence_of(operator);
+  }
+
+  return false;
+}
--- src/postfix-conversion.c
+++ src/postfix-conversion.c
@@ -5,76 +5,16 @@
 #include <string.h>
 
 #include "src/associativity.h"
-#include "src/infix-validation.h"
 #include "src/postfix-validation.h"
 #include "src/precedence.h"
 #include "src/stack.h"
 #include "src/token.h"
 #include "src/util.h"
 
-static bool stack_takes_precedence(const Stack *stack, const char *operator);
 static bool l_operand_needs_parens(const char *operand, const char *operator);
 static bool r_operand_needs_parens(const char *operand, const char *operator);
 static bool is_expression(const char *operand);
 static char *parenthesize(char *operand);
-
-int infix_to_postfix(char *postfix, const char *infix) {
-  if (postfix == NULL || infix == NULL) {
-    return RPN_NULL_POINTER_ERROR;
-  }
-
-  if (!is_valid_infix_expression(infix)) {
-    return RPN_MALFORMED_EXPRESSION_ERROR;
-  }
-
-  strcpy(postfix, "");
-
-  Stack *stack = NULL;
-
-  size_t infix_length = strlen(infix);
-
-  for (size_t i = 0; i < infix_length; i += 1) {
-    char token[2];
-    copy_substring(token, infix + i, 1);
-
-    if (is_operand(token)) {
-      strcat(postfix, token);
-    } else if (is_operator(token)) {
-      while (stack_takes_precedence(stack, token)) {
-        char operator[2];
-        pop(operator, &stack);
-        strcat(postfix, operator);
-      }
-
-      if (push(&stack, token) == NULL) {
-        clear(&stack);
-        return RPN_MEMORY_ALLOCATION_ERROR;
-      }
-    } else if (is_opening_paren(token)) {
-      if (push(&stack, token) == NULL) {
-        clear(&stack);
-        return RPN_MEMORY_ALLOCATION_ERROR;
-      }
-    } else if (is_closing_paren(token)) {
-      while (!is_opening_paren(peek(stack))) {
-        char operator[2];
-        pop(operator, &stack);
-        strcat(postfix, operator);
-      }
-
-      char opening_paren[2];
-      pop(opening_paren, &stack);
-    }
-  }
-
-  while (!is_empty(stack)) {
-    char operator[2];
-    pop(operator, &stack);
-    strcat(postfix, operator);
-  }
-
-  return RPN_SUCCESS;
-}
 
 int postfix_to_infix(char *infix, const char *postfix) {
   if (infix == NULL || postfix == NULL) {
@@ -131,20 +71,6 @@ int postfix_to_infix(char *infix, const char *postfix) {
   clear(&stack);
 
   return RPN_SUCCESS;
-}
-
-static bool stack_takes_precedence(const Stack *stack, const char *operator) {
-  assert(operator != NULL);
-
-  if (!is_empty(stack) && is_left_associative(operator)) {
-    return precedence_of(peek(stack)) >= precedence_of(operator);
-  }
-
-  if (!is_empty(stack) && is_right_associative(operator)) {
-    return precedence_of(peek(stack)) > precedence_of(operator);
-  }
-
-  return false;
 }
 
 static bool l_operand_needs_parens(const char *operand, const char *operator) {
